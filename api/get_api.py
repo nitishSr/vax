0b2ebd13105f96fd4355471ea643fd60dfaa0259
nitishSr <nsrivast@redhat.com>
2021-06-11 18:37:15 +0530

    Added templates and logic for date view split screen

--- api/get_api.py
+++ api/get_api.py
@@ -1,14 +1,17 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from config import config_data
 
 BASE_URL = config_data["base_url"]
 current_time = datetime.now()
 todays_date = current_time.strftime("%d-%m-%Y")
+tomorrow = current_time + timedelta(days=1)
+tomorrows_date = tomorrow.strftime("%d-%m-%Y")
+dates_to_search = [todays_date, tomorrows_date]
 
 
-def get_availability_by_pin(pincode, date=todays_date):
+def get_availability_by_pin(pincode, date):
     """
     Gets the availability by pincode and date
 
@@ -26,3 +29,31 @@ def get_availability_by_pin(pincode, date=todays_date):
         params=params,
     )
     return response
+
+
+def get_vaccine_slots(pincode):
+    """
+    Gets the availability by pincode for today's and tomorrow's date
+
+    Accepts:
+        pincode (str): Valid area pin code
+
+    Returns:
+        vaccine_date (list): List containing json data fetched from get api
+        msg_str (str): The message string containing errors if any
+    """
+    vaccine_data = []
+    msg_str = ""
+    try:
+        for date_search in dates_to_search:
+            res = get_availability_by_pin(pincode, date_search)
+            if res.status_code == 200:
+                fetched_data = res.json()
+                vaccine_data.append(fetched_data["sessions"])
+            else:
+                msg_str = "Slot availability data could not fetched, please try again in some time !\
+                    \n Server returned : {}".format(res.text)
+                break
+    except Exception as err:
+        msg_str = err
+    return vaccine_data, msg_str
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from api.get_api import get_availability_by_pin
+from api.get_api import get_availability_by_pin, get_vaccine_slots
 from flask import Flask, render_template, request, url_for, redirect, flash
 import secrets
 
@@ -8,26 +8,18 @@ app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    errors = []
+    msg_str = ""
     pincode = ""
-    data = {}
+    vaccine_slots_data = {}
     if request.method == "POST":
-        print("Inside")
         pincode = request.form["pincode"]
-        # date = request.form["date"]
-
-    if not pincode:
-        flash("Pincode is required !")
-    else:
-        try:
-            res = get_availability_by_pin(pincode)
-            if res.status_code == 200:
-                fetched_data = res.json()
-                data = fetched_data["sessions"]
-            else:
-                flash(
-                    "Slot availability data could not fetched, please try again in some time !"
-                )
-        except Exception:
-            errors.append("Unable to get vaccine data")
-    return render_template("index.html", errors=errors, data=data)
+        if not pincode:
+            flash("Pincode is required !")
+        else:
+            vaccine_slots_data, msg_str = get_vaccine_slots(pincode)
+    return render_template(
+        "index.html",
+        errors=msg_str,
+        data=vaccine_slots_data,
+        len=len(vaccine_slots_data),
+    )
